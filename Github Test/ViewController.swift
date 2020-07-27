b6c0274638334be68ecc4078a3220599572d5831
sana <starsm2020@gmail.com>
2020-07-27 13:24:10 -0700

    Initial Commit

---
+++ Github Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Github Test
+//
+//  Created by Sana Manesh on 7/27/20.
+//  Copyright © 2020 Sana Manesh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
